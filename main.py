bbd7803f4e858c2ff233b871ae76ff9a948211d7
Pranav K <pranav2005.kp@gmail.com>
2025-09-06 15:31:52 +0530

    Fetch Data and compute lag features to make prediction

--- main.py
+++ main.py
@@ -7,6 +7,13 @@ model = joblib.load("weather_model.pkl")
 label_encoder = joblib.load("label_encoder.pkl")
 
 app = FastAPI(title="Weather Prediction API", version="1.0")
+lag_features = [
+        "temperature_2m_mean (°C)", 
+        "relative_humidity_2m_mean (%)", 
+        "pressure_msl_mean (hPa)", 
+        "precipitation_sum (mm)", 
+        "cloud_cover_mean (%)"
+    ]
 
 # Home route
 @app.get("/")
@@ -18,3 +25,26 @@ def home():
 async def data():
     datas = fetch_data(52.52, 13.41, 8)
     return {"data":datas.to_dict(orient="records")}
+
+@app.get("/predict")
+async def predict(lat: float = 52.52, lon: float = 13.41, days: int = 8):
+    datas = fetch_data(lat, lon, days + 7)  # fetch extra days for lag7
+
+    for col in lag_features:
+        datas[f"{col}_lag1"] = datas[col].shift(1)
+        datas[f"{col}_lag7"] = datas[col].shift(7)
+    for col in lag_features:
+        datas[f"{col}_rollmean3"] = datas[col].shift(1).rolling(3).mean()
+
+    datas = datas.dropna().reset_index(drop=True)
+    latest_row = datas.iloc[[-1]]
+
+    predictors = [col for col in latest_row.columns if col != "date"]
+    X = latest_row[predictors].to_numpy()
+    y_pred = model.predict(X)
+    y_pred_label = label_encoder.inverse_transform(y_pred)[0]
+
+    return {
+        "date": str(latest_row["date"].values[0]),
+        "prediction": str(y_pred_label)
+    }
--- models.py
+++ models.py
@@ -8,13 +8,6 @@ cache_session = requests_cache.CachedSession('.cache', expire_after = 3600)
 retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
 openmeteo = openmeteo_requests.Client(session = retry_session)
 
-lag_features = [
-    "temperature_2m_mean (°C)", 
-    "relative_humidity_2m_mean (%)", 
-    "pressure_msl_mean (hPa)", 
-    "precipitation_sum (mm)", 
-    "cloud_cover_mean (%)"
-]
 
 # Fetch the past 8 days data
 def fetch_data(lat: float, lon: float, days: int = 8) ->pd.DataFrame:
@@ -23,11 +16,13 @@ def fetch_data(lat: float, lon: float, days: int = 8) ->pd.DataFrame:
         "latitude" : lat,
         "longitude" : lon,
         "daily" : [
-            "temperature_2m_mean",
-            "relative_humidity_2m_mean",
-            "pressure_msl_mean",
-            "precipitation_sum",
-            "cloud_cover_mean"
+            'cloud_cover_mean',
+            'relative_humidity_2m_mean',
+            'pressure_msl_mean',
+            'cloud_cover_max',
+            'dew_point_2m_mean',
+            'temperature_2m_mean',
+            'precipitation_sum'
         ],
         "timezone" : "auto",
         "start_date" : (pd.Timestamp.today().normalize() - pd.Timedelta(days=days)).strftime("%Y-%m-%d"),
@@ -37,11 +32,14 @@ def fetch_data(lat: float, lon: float, days: int = 8) ->pd.DataFrame:
     response = responses[0]
 
     daily = response.Daily()
-    temperature_2m_mean = daily.Variables(0).ValuesAsNumpy()
+    cloud_cover_mean = daily.Variables(0).ValuesAsNumpy()
     relative_humidity_2m_mean = daily.Variables(1).ValuesAsNumpy()
     pressure_msl_mean = daily.Variables(2).ValuesAsNumpy()
-    precipitation_sum = daily.Variables(3).ValuesAsNumpy()
-    cloud_cover_mean = daily.Variables(4).ValuesAsNumpy()
+    cloud_cover_max = daily.Variables(3).ValuesAsNumpy()
+    dew_point_2m_mean = daily.Variables(4).ValuesAsNumpy()
+    temperature_2m_mean = daily.Variables(5).ValuesAsNumpy()
+    precipitation_sum = daily.Variables(6).ValuesAsNumpy()
+
 
     dates = pd.date_range(
         start=pd.to_datetime(daily.Time(), unit="s", utc=True),
@@ -51,11 +49,13 @@ def fetch_data(lat: float, lon: float, days: int = 8) ->pd.DataFrame:
 
     df = pd.DataFrame({
         "date": dates,
-        "temperature_2m_mean (°C)": temperature_2m_mean,
+        "cloud_cover_mean (%)": cloud_cover_mean,
         "relative_humidity_2m_mean (%)": relative_humidity_2m_mean,
         "pressure_msl_mean (hPa)": pressure_msl_mean,
+        "cloud_cover_max (%)" :cloud_cover_max,
+        "dew_point_2m_mean (°C)" : dew_point_2m_mean,
+        "temperature_2m_mean (°C)": temperature_2m_mean,
         "precipitation_sum (mm)": precipitation_sum,
-        "cloud_cover_mean (%)": cloud_cover_mean
     })
 
     return df.reset_index(drop=True)
